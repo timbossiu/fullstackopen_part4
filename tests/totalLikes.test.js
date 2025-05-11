9031478adbc07fee5cd0fae5268a2b79c8cc97c2
Tim Alderkot <timalderkot@gmail.com>
2025-05-11 17:30:03 +0200

    4.4 and 4.5 finished, started with 4.6

--- tests/totalLikes.test.js
+++ tests/totalLikes.test.js
@@ -15,9 +15,38 @@ describe('total likes', () => {
         __v: 0
       }
     ]
+
+    const listWithTwoBlogs = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5,
+        __v: 0
+      },
+      {
+        _id: '3a422aa71b54a676234d17f8',
+        title: 'Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: '',
+        likes: 39,
+        __v: 0
+      }
+    ]
   
     test('when list has only one blog, equals the likes of that', () => {
       const result = totalLikes(listWithOneBlog)
-      assert.strictEqual(result, 5)
+      assert.deepStrictEqual(result, 5)
+    }),
+    
+    test('when list has two blogs, counts the likes of that', () => {
+      const result = totalLikes(listWithTwoBlogs)
+      assert.deepStrictEqual(result, 44)
+    }),
+    
+    test('when list has 0 blogs, returns 0 likes', () => {
+      const result = totalLikes([])
+      assert.deepStrictEqual(result, 0)
     })
   })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,6 @@
+// Load the full build.
+var _ = require('lodash');
+
 const dummy = (blogs) => {
     return 1;
 }
@@ -6,9 +9,29 @@ const totalLikes = (blogs) => {
     const reducer = (sum, blog) => {
         return sum + blog.likes
     }
+
+    return blogs.length === 0 ?
+        0 :
+        blogs.reduce(reducer, 0)
 }
+
+const favouriteBlog = (blogs) => {
+    const reducer = (max, current) => {
+        return (max.likes > current.likes ? max : current)
+    }
+
+    return blogs.length === 0 ?
+        null : blogs.reduce(reducer, blogs.at(0))
+}
+
+const mostBlogs = (blogs) => {
+    return _.groupBy(blogs,'authors')
+}
+
   
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favouriteBlog,
+    mostBlogs
 }
