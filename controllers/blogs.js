12cdbac80fef632f91013a3800518f946d729ad1
Tim Alderkot <timalderkot@gmail.com>
2025-06-17 12:57:52 +0200

    done with 4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
     findedBlogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -10,14 +20,22 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body)
 
-    foundUser = await User.findOne().sort({ field: 'asc', _id: -1 })
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
 
-    blog.user = foundUser
+    const user = await User.findById(decodedToken.id)
+
+    if (!user) {
+      return response.status(400).json({ error: 'userId missing or not valid' })
+    }
+
+    blog.user = user
     savedBlog = await blog.save()
 
-    console.log(foundUser)
-    foundUser.blogs = foundUser.blogs.concat(savedBlog.id)
-    await foundUser.save()
+    user.blogs = user.blogs.concat(savedBlog.id)
+    await user.save()
 
     if (savedBlog) {
       response.status(201).json(savedBlog)
